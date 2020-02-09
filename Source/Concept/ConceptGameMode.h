b2a676e4c7a63ebbce76ba341870c088d4433ef9
CoryBrown-42 <yroc.999@gmail.com>
2020-02-09 12:51:14 -0500

    Setup for first assignment

---
+++ Source/Concept/ConceptGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ConceptGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AConceptGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AConceptGameMode();
+};
+
+
+
